85b45a18a63a4a5b8b3d360c01f6540aa94373a6
Mateus Furtado <mateus2013furt@gmail.com>
2024-06-24 17:45:18 -0300

    first commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <main>
+
+    <header class="flex">
+    
+      <div class="cartao">
+        <h1>Bartira Rocha</h1>
+        <p>Professora da UERN-NATAL</p>
+      </div>   
+      <div class="foto">
+        <img src="@/assets/fotobartira.gif" alt="logo">
+      </div>
+    </header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  
+    <RouterView />
+    
+  </main>
+  
+</template>
+
+<style scoped>
+main{
+  margin: 5%;
+}
+.foto{
+  width: 400px; /* Largura do contêiner */
+  height: 525px; /* Altura do contêiner */
+  overflow: hidden; /* Esconde partes da imagem que excedem o contêiner */
+  position: relative;
+
+}
+.foto img{
+  width: 500px;
+  height: 525px;
+  position: absolute; /* Ajusta a posição vertical da imagem */
+  left: -50px; /* Ajusta a posição horizontal da imagem */
+}
+.cartao{
+  font-family: 'Times New Roman', Times, serif;
+  text-align: center;
+  background-color: rgb(255, 255, 255);
+  width: 60%;
+  height: 360px;
+  position: absolute;
+  top: 155px;  /* Ajuste conforme necessário */
+  left: 32%; /* Ajuste conforme necessário */
+  z-index: 10; /* Garante que este componente esteja sobreposto */
+}
+.flex{
+  display: flex;
+}
+</style>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!--<TheWelcome />-->
+    <p>2016 - 2020
+      Doutorado em Sistemas e Computação.
+      Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+      Título: Um Modelo Semântico de Dados para Cidades Inteligentes
+      Ano de obtenção: 2020.
+      Orientador: Thais Vasconcelos Batista. <br>
+      
+      1999 - 2000
+      Mestrado em Sistemas e Computação.
+      Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+      Título: A Linguagem de Descrição de Arquiteturas ZCLcsp
+      Ano de Obtenção: 2000.
+      Orientador: David Boris Paul Déharbe. <br>
+      
+      
+      1993 - 1998
+      Graduação em Ciências da Computação.
+      Universidade Federal do Rio Grande do Norte, UFRN, Brasil.
+      </p>
+  </main>
+</template>
