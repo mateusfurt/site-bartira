ee4ec2c1aafd9895d2c7a748afdbf1ac2dca087a
Mateus Furtado <mateus2013furt@gmail.com>
2024-06-25 21:27:06 -0300

    cartão pronto

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <header class="flex">
     
       <div class="cartao">
-        <h1 class="nomecartao">Bartira Rocha</h1>
+        <h1 class="nomecartao">DRa. Bartira Rocha</h1>
         <h3 class="profissao">Professora de Ciência da Computação</h3>
         <div class="basecartao flex">
           <div>
@@ -42,7 +42,17 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 main{
-  margin: 60px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  
+}
+header{
+  margin: 0px;
+  padding: 0px;
 }
 h3{
   font-size: 23px;
@@ -70,33 +80,63 @@ h3{
   text-shadow: 7px 7px 5px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
 }
+
+.foto img{
+  width: 500px;
+  height: 525px;
+  position: relative; /* Ajusta a posição vertical da imagem */
+  left: -50px; /* Ajusta a posição horizontal da imagem */
+}
 .foto{
-  margin-left: 30px;
   width: 400px; /* Largura do contêiner */
   height: 525px; /* Altura do contêiner */
   overflow: hidden; /* Esconde partes da imagem que excedem o contêiner */
   position: relative;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
-
-}
-.foto img{
-  width: 500px;
-  height: 525px;
-  position: absolute; /* Ajusta a posição vertical da imagem */
-  left: -50px; /* Ajusta a posição horizontal da imagem */
+  left: -840px;
 }
 .cartao{
   font-family: 'Times New Roman', Times, serif;
   text-align: left;
   padding-left: 40px;
   background-color: rgb(255, 255, 255);
-  width: 800px;
+  width: 830px;
   height: 340px;
-  position: absolute;
-  top: 170px;  /* Ajuste conforme necessário */
-  left: 440px; /* Ajuste conforme necessário */
+  position: relative;
+  top: 130px;  /* Ajuste conforme necessário */
+  left: 400px; /* Ajuste conforme necessário */
   z-index: 10; /* Garante que este componente esteja sobreposto */
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+}
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  background-color: blue;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+  width: 60px;
+  height: 30px;
+  padding: 0;
+  align-content: center;
+  margin: 0;
+  
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 .flex{
   display: flex;
